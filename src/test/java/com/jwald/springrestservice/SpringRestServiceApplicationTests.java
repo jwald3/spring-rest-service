9f6932890ed434e6ddb7b858be860edaf5ec52dd
Jordan <jwald3@brockport.edu>
2022-04-04 20:31:52 -0400

    Initial commit

---
+++ src/test/java/com/jwald/springrestservice/SpringRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwald.springrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
